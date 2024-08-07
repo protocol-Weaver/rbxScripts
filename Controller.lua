842e7d815496ccfa649bbc007f2eec2eda9612b2
Protocol Cyberia <98874867+protocol-Weaver@users.noreply.github.com>
2024-08-07 09:29:31 +0500

    Update Controller.lua

--- Controller.lua
+++ Controller.lua
@@ -36,12 +36,108 @@ local function TrailConnector()
     trailAttachment1.Parent = humanoidRootPart
 end
 
--- Plays The Dash Animation
+
+
+-- Sets the size of a SpecialMesh object
+local function SetVectorToSpecialMesh(SpecialMesh, Size)
+    -- Get the parent part of the SpecialMesh
+    local Part = SpecialMesh.Parent
+    -- Calculate the original size of the part based on the SpecialMesh scale
+    local OriginalSize = Part.Size / SpecialMesh.Scale
+    -- Set the new size of the part
+    Part.Size = Size
+    -- Adjust the SpecialMesh scale to match the new size
+    SpecialMesh.Scale = Size / OriginalSize
+end
+
+-- Initializes the slash effect
+local function SlashInit()
+    -- Get the sword tool from the StarterPack
+    local sword = game:GetService("StarterPack").Tool
+    -- Clone the Slash mesh from storage
+    local SlashMesh = storage.Slash:Clone()
+    -- Set the vertex color of the Slash mesh
+    local VertexColor = Vector3.new(10, 0, 0)
+    -- Define the size of the Slash mesh
+    local Size = Vector3.new(13, 1, 12)
+    -- Define the initial CFrame value
+    local CFrameVal = CFrame.new(0, 0, 0)
+    -- Define the start rotation
+    local StartRotation = CFrame.Angles(0, 0, 0)
+    -- Define the rotation amount
+    local RotationAmount = CFrame.Angles(0, -90, -90)
+    -- Define the time interval for animation images
+    local Time = 0.05
+    -- Set the parent of the Slash mesh to the workspace
+    local Parent = workspace
+    -- Define the tween rotation info
+    local TweenRotationInfo = TweenInfo.new(0.3)
+    -- Define the tween rotational goal
+    local TweenRotationalGoal = {Value = RotationAmount}
+    -- List of animation image asset IDs
+    local AnimationImages = {
+        "rbxassetid://18357231142",
+        "rbxassetid://18357230843",
+        "rbxassetid://18356887208",
+        "rbxassetid://18356887551",
+    }
+    -- Set the vector to the Slash mesh
+    SetVectorToSpecialMesh(SlashMesh.Mesh, Size)
+    -- Set the CFrame of the Slash mesh
+    SlashMesh.CFrame = CFrameVal
+    -- Set the vertex color of the Slash mesh
+    SlashMesh.Mesh.VertexColor = VertexColor
+    -- Set the parent of the Slash mesh
+    SlashMesh.Parent = Parent
+    
+    -- Create a new CFrameValue for rotation
+    local RotationValue = Instance.new("CFrameValue")
+    -- Create and play the tween for rotation
+    TweenService:Create(RotationValue, TweenRotationInfo, TweenRotationalGoal):Play()
+    -- Connect the RenderStepped event to update the Slash mesh CFrame
+    local MoveConnection = game:GetService("RunService").RenderStepped:Connect(function()
+        SlashMesh.CFrame = character:GetPivot() * StartRotation * RotationValue.Value
+    end)
+    -- Spawn a new task to handle the animation images
+    task.spawn(function()
+        for _i, textures in pairs(AnimationImages) do 
+            -- Set the texture ID of the Slash mesh
+            SlashMesh.Mesh.TextureId = textures    
+            -- Wait for the specified time interval
+            task.wait(Time)
+        end
+        -- Disconnect the RenderStepped event
+        MoveConnection:Disconnect()
+        -- Destroy the rotation value and Slash mesh
+        RotationValue:Destroy()
+        SlashMesh:Destroy()
+    end)
+end
+
+-- Plays the slash animation
+local function Slash()
+    -- Create a new animation instance
+    local Animation = Instance.new("Animation")
+    -- Set the animation ID
+    Animation.AnimationId = "rbxassetid://18419828546"
+    -- Load the animation onto the humanoid animator
+    local Track = humanoid.Animator:LoadAnimation(Animation)
+    -- Play the animation
+    Track:Play()
+    -- Wait for the animation to stop
+    Track.Stopped:Wait()
+    -- Initialize the slash effect
+    SlashInit()
+end
+
+
+
+-- Using Linear Velocity to Create A Movement And playing movement animation and using trail effect to display A Dash.
 local function Dash()
-    -- Creates Dash Effect
+    -- Creates Dash Effect by connecting trail attachments
     TrailConnector()
     
-    -- Clone the dash trail and set its attachments
+    -- Clone the dash trail and set its attachments to the humanoid root part
     local trail = dash.Trail:Clone()
     trail.Attachment0 = humanoidRootPart.TrailAttachment0
     trail.Attachment1 = humanoidRootPart.TrailAttachment1
@@ -61,10 +157,11 @@ local function Dash()
     -- Wait for a short duration before destroying the objects
     task.wait(0.1)
     
-    -- Destroy the linear velocity and trail objects
+    -- Destroy the linear velocity and trail objects to clean up
     linearVelocity:Destroy()
     trail:Destroy()
 end
+
 
 -- Heals The Player Health
 local function Heal()
@@ -96,14 +193,21 @@ local camera = workspace.CurrentCamera
 local flying = false
 local isjumping = false
 
--- This Makes the Body Able to Fly 
+-- Create a new BodyVelocity instance
 local bodyVelocity = Instance.new("BodyVelocity")
+-- Create a new BodyGyro instance
 local bodyGyro = Instance.new("BodyGyro")
 
-bodyVelocity.MaxForce = Vector3.new(1,1,1)* 10^6
+-- Set the maximum force for the BodyVelocity to a high value
+bodyVelocity.MaxForce = Vector3.new(1, 1, 1) * 10^6
+-- Set the power (P) value for the BodyVelocity
 bodyVelocity.P = 10^6
-bodyGyro.MaxTorque = Vector3.new(1,1,1)* 10^6
+
+-- Set the maximum torque for the BodyGyro to a high value
+bodyGyro.MaxTorque = Vector3.new(1, 1, 1) * 10^6
+-- Set the power (P) value for the BodyGyro
 bodyGyro.P = 10^6
+
 
 -- Toggles From Jumping To Flying Or Flying to Jumping
 local function stateChange(old, new)
